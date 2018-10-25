fd39f1f0aa821d8c788ac3941c0c3dc033950556
Marlon Mazzine dos Santos Figueiredo <44420680+MarlonMazzine@users.noreply.github.com>
2018-10-25 13:04:01 -0300

    rapezoidal-rule
    Integral(x * cos(x^2))

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/*Metodo de Simpson para a integral de 'x * cos x^2'*/
+/*Metodo de Trapezio para a funcao de 'x * cos x^2'*/
 
 #include <iostream>
 #include <math.h>
@@ -8,62 +8,53 @@ using namespace std;
 
 int main()
 {
-    int n, contador = 0;
-    cout << "\nCLACULADORA INTEGRAL POR METODO DE SIMPSON\n";
-    cout << "\n==========================================\n\n";
+    int n, contador = 1;
+    cout << "\n\nCLACULADORA INTEGRAL POR METODO DE TRAPEZIO\n";
+    cout << "\n===========================================\n\n";
     do{
-    inicio2:
-        cout << "Entre com um numero par de interacoes: ";
+    inicio:
+        cout << "Digite o numero de interacoes: ";
         cin >> n;
 
-        if(n % 2 == 0){
-            double valor = (0 - PI) / n;
-            double x[n + 1], xProx = 0;
-            for(int i = 0; i < (n + 1); i++){
-                if(i == 0){
-                    x[i] = 0;
-                }else if(i > 0 && i < (n + 1)){
-                    x[i] = xProx + valor;
-                }
-                xProx = x[i];
+        double valor = (0 - PI) / n;
+        double x[n + 1], xProx = 0;
+
+        for(int i = 0; i < (n + 1); i++){
+            if(i == 0){
+                x[i] = 0;
+            }else if(i > 0 && i < (n + 1)){
+                x[i] = valor + xProx;
             }
-
-            double somatorio = 0;
-            double fX[n + 1];
-            double quadradoDeX = 0, cosDoQuadrado = 0, multCosQuad = 0;
-
-            for(int i = 0; i < (n + 1); i++){
-                if(i == 0){
-                    fX[i] = x[i];
-                }else if(i % 2 == 1 && i < n){
-                    quadradoDeX = pow(x[i], 2);
-                    cosDoQuadrado = cos(quadradoDeX);
-                    multCosQuad = cosDoQuadrado * x[i];
-                    fX[i] = 4 * multCosQuad;
-                }else if(i % 2 == 0 && i < n){
-                    quadradoDeX = pow(x[i], 2);
-                    cosDoQuadrado = cos(quadradoDeX);
-                    multCosQuad = cosDoQuadrado * x[i];
-                    fX[i] = 2 * multCosQuad;
-                }else{
-                    quadradoDeX = pow(x[i], 2);
-                    cosDoQuadrado = cos(quadradoDeX);
-                    multCosQuad = cosDoQuadrado * x[i];
-                    fX[i] = multCosQuad;
-                }
-                somatorio = fX[i] + somatorio;
-            }
-
-            double resultado = 0.3 * (valor * somatorio);
-            cout << "\n==========================================\n\n";
-            printf("O resultado para %d interacoes foi de %.5f\n", n, resultado);
-            cout << "\n==========================================\n\n";
-        }else{
-            cout << "\n===\n\nO valor de interacoes deve ser par.\n\n===\n\n";
-            goto inicio2;
-            continue;
+            xProx = x[i];
         }
 
-    }while(contador == 0);
+        double somatorio = 0;
+        double fX[n + 1];
+        double quadradoDeX = 0, cosDoQuadrado = 0, multCosQuad = 0;
+
+        for(int i = 0; i < (n + 1); i++){
+            if(i == 0){
+                fX[i] = x[i];
+            }else if(i > 0 && i < n){
+                quadradoDeX = pow(x[i], 2);
+                cosDoQuadrado = cos(quadradoDeX);
+                multCosQuad = cosDoQuadrado * x[i];
+                fX[i] = 2 * multCosQuad;
+            }else{
+                quadradoDeX = pow(x[i], 2);
+                cosDoQuadrado = cos(quadradoDeX);
+                multCosQuad = cosDoQuadrado * x[i];
+                fX[i] = multCosQuad;
+            }
+            somatorio = fX[i] + somatorio;
+        }
+
+        double resultado = 0.5 * (valor * somatorio);
+        cout << "\n===========================================\n\n";
+        printf("O resultado para %d interacoes foi de %.5f.\n", n, resultado);
+        cout << "\n===========================================\n\n";
+
+    }while(contador == 1);
+
     return 0;
 }
