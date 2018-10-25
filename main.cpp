e36a44c06a2b4810f7db602e52525defdec98174
Marlon Mazzine dos Santos Figueiredo <44420680+MarlonMazzine@users.noreply.github.com>
2018-10-25 13:01:06 -0300

    simpson
    Simpson to expression: Integral(x * cos(x^2)).

--- main.cpp
+++ main.cpp
@@ -1,60 +1,69 @@
+/*Metodo de Simpson para a integral de 'x * cos x^2'*/
+
 #include <iostream>
 #include <math.h>
+#define PI 3.14159
 
 using namespace std;
 
 int main()
 {
-    int contador = 1;
-    while(contador != 0){
+    int n, contador = 0;
+    cout << "\nCLACULADORA INTEGRAL POR METODO DE SIMPSON\n";
+    cout << "\n==========================================\n\n";
+    do{
+    inicio2:
+        cout << "Entre com um numero par de interacoes: ";
+        cin >> n;
 
-        int n1, n2;
-        cout << "TRANSFORMACAO LINEAR DE 'N' NUMEROS\n";
-        cout << "==============================================\n";
-        cout << "Digite o tamanho de 'u': ";
-        cin >> n1;
-        cout << "Digite o tamanho de 'v': ";
-        cin >> n2;
-        cout << "==============================================\n";
-
-        if(n1 == n2){
-
-            double u[n1];
-            double v[n2];
-            double tamanhoDeU = 0,
-                   tamanhoDeV = 0,
-                   produtoEscalar = 0;
-
-            for(int i = 0; i < n1 && i < n2; i++){
-                cout << "Digite o valor de 'u' na posicao " << i << ": ";
-                cin >> u[i];
-                cout << "Digite o valor de 'v' na posicao " << i << ": ";
-                cin >> v[i];
-                produtoEscalar += u[i] * v[i];
-                tamanhoDeU += u[i] * u[i];
-                tamanhoDeV += v[i] * v[i];
+        if(n % 2 == 0){
+            double valor = (0 - PI) / n;
+            double x[n + 1], xProx = 0;
+            for(int i = 0; i < (n + 1); i++){
+                if(i == 0){
+                    x[i] = 0;
+                }else if(i > 0 && i < (n + 1)){
+                    x[i] = xProx + valor;
+                }
+                xProx = x[i];
             }
 
-            tamanhoDeU = sqrt(tamanhoDeU);
-            tamanhoDeV = sqrt(tamanhoDeV);
+            double somatorio = 0;
+            double fX[n + 1];
+            double quadradoDeX = 0, cosDoQuadrado = 0, multCosQuad = 0;
 
-            //cout << "\n\n" << produtoEscalar;
+            for(int i = 0; i < (n + 1); i++){
+                if(i == 0){
+                    fX[i] = x[i];
+                }else if(i % 2 == 1 && i < n){
+                    quadradoDeX = pow(x[i], 2);
+                    cosDoQuadrado = cos(quadradoDeX);
+                    multCosQuad = cosDoQuadrado * x[i];
+                    fX[i] = 4 * multCosQuad;
+                }else if(i % 2 == 0 && i < n){
+                    quadradoDeX = pow(x[i], 2);
+                    cosDoQuadrado = cos(quadradoDeX);
+                    multCosQuad = cosDoQuadrado * x[i];
+                    fX[i] = 2 * multCosQuad;
+                }else{
+                    quadradoDeX = pow(x[i], 2);
+                    cosDoQuadrado = cos(quadradoDeX);
+                    multCosQuad = cosDoQuadrado * x[i];
+                    fX[i] = multCosQuad;
+                }
+                somatorio = fX[i] + somatorio;
+            }
 
-            float pi = 3.1415,
-                  radiano = 0,
-                  anguloEmGraus = 0;
-            int grau = 180;
-
-            radiano = produtoEscalar / (tamanhoDeU * tamanhoDeV);
-
-            anguloEmGraus = acos(radiano) * grau / pi;
-
-            cout << "==============================================\n";
-            cout << "\n\n O ângulo " << radiano << " radianos em graus e' = "<< anguloEmGraus << " graus" << endl;
+            double resultado = 0.3 * (valor * somatorio);
+            cout << "\n==========================================\n\n";
+            printf("O resultado para %d interacoes foi de %.5f\n", n, resultado);
+            cout << "\n==========================================\n\n";
         }else{
-            cout << "O tamanho de 'u' não pode ser diferente do tamanho de 'v'.\n";
-            cout << "==============================================\n";
+            cout << "\n===\n\nO valor de interacoes deve ser par.\n\n===\n\n";
+            goto inicio2;
+            continue;
         }
-    }
+
+    }while(contador == 0);
     return 0;
 }
